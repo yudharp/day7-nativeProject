906ffaabd3923417f10f2e8a922f0c1f295fde54
TiofennyAngelina <71843405+TiofennyAngelina@users.noreply.github.com>
2024-02-06 09:19:37 +0700

    DetailScreen

---
+++ src/screens/DetailScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const DetailScreen = () => {
+    return (
+        <View style={{flex: 1, justifyContent: 'center', alignItem: 'center'}}>
+            <Text>DetailsScreen</Text>
+        </View>
+    );
+};
+
+export default DetailScreen;

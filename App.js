4f1aa4ef7b229f4d40e8a541c31746665ba8e2c1
tyaasdwi <dwityas01@gmail.com>
2024-02-06 09:28:43 +0700

    udah ya

---
+++ App.js
@@ -0,0 +1,22 @@
+// App.js
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native'; 
+import { createStackNavigator } from '@react-navigation/stack'; 
+import HomeScreen from './src/screen/HomeScreen';
+import DetailsScreen from './src/screen/DetailsScreen';
+
+const Stack = createStackNavigator();
+
+
+const App = () => {
+return (
+
+  <NavigationContainer>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Details" component={DetailsScreen} /> 
+    </Stack.Navigator>
+  </NavigationContainer>
+);
+};
+export default App;

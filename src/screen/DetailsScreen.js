4fb6ec65bf29a493bbdb84c9584047b8cce8fc3f
yudharp <yudhaananda.rp@gmail.com>
2024-02-06 15:05:46 +0700

    add components user

--- src/screen/DetailsScreen.js
+++ src/screen/DetailsScreen.js
@@ -1,20 +1,14 @@
 // components/DetailsScreen.js
 import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import User from '../components/User'; // Import komponen User
 
 const DetailsScreen = ({ route }) => {
   const { user } = route.params;
 
   return (
     <View style={styles.container}>
-      <Image source={{ uri: user.picture.large }} style={styles.thumbnail} />
-      <View style={styles.userInfo}>
-        <Text>{`${user.name.first} ${user.name.last}`}</Text>
-        <Text>Email: {user.email}</Text>
-        <Text>Gender: {user.gender}</Text>
-        <Text>Phone: {user.phone}</Text>
-        {/* Add more details as needed */}
-      </View>
+      <User user={user} isDetailPage={true} pressItem={() => {}} />
     </View>
   );
 };
@@ -24,15 +18,6 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     padding: 20,
-  },
-  thumbnail: {
-    width: 150,
-    height: 150,
-    borderRadius: 75,
-    marginBottom: 20,
-  },
-  userInfo: {
-    alignItems: 'center',
   },
 });
 
